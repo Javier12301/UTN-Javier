99237e16f2ffe0b3abcfee3bfebacc8e9b941891
Javier12301 <javierramirez1230123@gmail.com>
2024-04-24 13:54:46 -0300

    Update

--- UTN/TP1/Testing/trabajos/TP1.py
+++ UTN/TP1/Testing/trabajos/TP1.py
@@ -1,31 +1,23 @@
 import re
+# INTEGRANTES
+"""
+Da costa Bonetti Nicolás - 41.203.867 - 414563 - 1K13
 
-# Código postal - Detección de país destino
-# LNNNNLLL - ARGENTINA - NO SE USA I ni O
-# Explicación de patron:
-# Teniendo en cuenta el abcedario:
- # 1. De 'A' a 'H' (ambas incluidas), ya que 'I' no está permitida. -> [A,B,C,D,E...,H]
- # 2. De 'J' a 'N' (ambas incluidas), ya que después de 'N' sigue 'O', que no está permitida.
- # 3. De 'P' a 'Z' (ambas incluidas), ya que todas estas letras están permitidas.
-patron_argentina = r'^[A-HJ-NP-Za-hj-np-z]{1}[0-9]{4}[A-Za-z]{3}$'
+Ramirez Javier - 44.608.055 - 414638 - 1K13
 
-# NNNN - Bolivia
-patron_bolivia = r'^[0-9]{4}$'
-# NNNNN-NNN - Brasil -> el guion debe estár includi
-patron_brasil = r'^[0-9]{5}-[0-9]{3}$'
-# NNNNNNN - Chile
-patron_chile = r'^[0-9]{7}$'
-# NNNNNN - Paraguay
-patron_paraguay = r'^[0-9]{6}$'
-# NNNNN - Uruguay
-patron_uruguay = r'^[0-9]{5}$'
+Villanueva Tomás - 46.033.827 - 405514 - 1K13
+
+Ambrosino Ignacio - 46.848.058 - 414663 - 1K13
+
+Molina Jonathan -  87298 - 1K13
+"""
+# Sistema Simple de Gestión de Envíos por Correo
 
 # Entrada de datos
 cp = input("Ingrese el código postal del lugar de destino: ")
 direccion = input("Dirección del lugar de destino: ")
 tipo = int(input("Tipo de envío (id entre 0 y 6 - ver tabla 2 en el enunciado): "))
 pago = int(input("Forma de pago (1: efectivo - 2: tarjeta): "))
-# Procesamiento
 
 # Variables finales
 destino = ""
@@ -33,83 +25,82 @@ provincia = ""
 inicial = 0
 final = 0
 
-if re.match(patron_argentina, cp):
-    print("El país destino es Argentina")
-    # Detección provincias
-    destino = "Argentina"
-    if(cp[0] == "C"):
+# Procesamiento
+if len(cp) == 8 and cp[0].isalpha and cp[0] != "O" and cp[0] != "I" and cp[1:4].isdigit() and [cp[5:].isalpha]:
+      # Detección provincias
+      destino = "Argentina"
+      if(cp[0] == "C"):
         provincia = "Ciudad Autónoma de Buenos Aires"
-    elif(cp[0] == "B"):
+      elif(cp[0] == "B"):
         provincia = "Buenos Aires"
-    elif(cp[0]== "K"):
+      elif(cp[0]== "K"):
         provincia = "Catamarca"
-    elif(cp[0] == "H"):
+      elif(cp[0] == "H"):
         provincia = "Chaco"
-    elif(cp[0] == "U"):
+      elif(cp[0] == "U"):
         provincia = "Chubut"
-    elif(cp[0] == "X"):
+      elif(cp[0] == "X"):
         provincia = "Córdoba"
-    elif(cp[0] == "W"):
+      elif(cp[0] == "W"):
         provincia = "Corrientes"
-    elif(cp[0] == "E"):
+      elif(cp[0] == "E"):
         provincia = "Entre Ríos"
-    elif(cp[0] == "P"):
+      elif(cp[0] == "P"):
         provincia = "Formosa"
-    elif(cp[0] == "Y"):
+      elif(cp[0] == "Y"):
         provincia = "Jujuy"
-    elif(cp[0] == "L"):
+      elif(cp[0] == "L"):
         provincia = "La Pampa"
-    elif(cp[0] == "F"):
+      elif(cp[0] == "F"):
         provincia = "La Rioja"
-    elif(cp[0] == "M"):
+      elif(cp[0] == "M"):
         provincia = "Mendoza"
-    elif(cp[0] == "N"):
+      elif(cp[0] == "N"):
         provincia = "Misiones"
-    elif(cp[0] == "Q"):
+      elif(cp[0] == "Q"):
        provincia = "Neuquén"
-    elif(cp[0] == "R"):
+      elif(cp[0] == "R"):
         provincia = "Rio Negro"
-    elif(cp[0] == "A"):
+      elif(cp[0] == "A"):
         provincia = "Salta"
-    elif(cp[0] == "J"):
+      elif(cp[0] == "J"):
         provincia = "San Juan"
-    elif(cp[0] == "D"):
+      elif(cp[0] == "D"):
         provincia = "San Luis"
-    elif(cp[0] == "Z"):
+      elif(cp[0] == "Z"):
         provincia = "Santa Cruz"
-    elif(cp[0] == "S"):
+      elif(cp[0] == "S"):
         provincia = "Santa Fe"
-    elif(cp[0] == "G"):
+      elif(cp[0] == "G"):
         provincia = "Santiago del Estero"
-    elif(cp[0] == "V"):
+      elif(cp[0] == "V"):
         provincia = "Tierra del Fuego"
-    elif(cp[0] == "T"):
+      elif(cp[0] == "T"):
         provincia = "Tucumán"
-    else:
-        print("Ocurrió un error al identificar la provincia")
-    print(f"Provincia/Ciudad: {provincia}")
-elif re.match(patron_bolivia, cp):
-    destino = "Bolivia"
-    provincia = "No aplica"
-elif re.match(patron_brasil, cp):
-    destino = "Brasil"
-    provincia = "No aplica"
-elif re.match(patron_chile, cp):
-    print("El país destino es Chile")
-    destino = "Chile"
-    provincia = "No aplica"
-elif re.match(patron_paraguay, cp):
-    print("El país destino es Paraguay")
-    destino = "Paraguay"
-    provincia = "No aplica"
-elif re.match(patron_uruguay, cp):
-    print("El país destino es Uruguay")
-    destino = "Uruguay"
-    provincia = "No aplica"
+elif len(cp) == 4 and cp[:4].isdigit():
+  destino = "Bolivia"
+  provincia = "No aplica"
+  
+elif len(cp) == 9 and cp[:4].isdigit() and cp[5] == "-" and cp[6:8].isdigit():
+  destino = "Brasil"
+  provincia = "No aplica"
+  
+elif len(cp) == 7 and cp[:6].isdigit():
+  destino = "Chile"
+  provincia = "No aplica"
+  
+elif len(cp) == 6 and cp[:5].isdigit():
+  destino = "Paraguay"
+  provincia = "No aplica"
+  
+elif len(cp) == 5 and cp[:4].isdigit():
+  destino = "Uruguay"
+  provincia = "No aplica"
+  
 else:
-    destino = "Otro"
-    provincia = "No aplica"
-    
+  destino = "Otro"
+  provincia = "No aplica"
+  
 # Código de tipo de envio
 #[0, 6]
 if(tipo >= 0 and tipo <= 6):
@@ -126,10 +117,7 @@ if(tipo >= 0 and tipo <= 6):
     elif(tipo == 5):
         inicial = 14300    
     elif(tipo == 6):
-        inicial = 17900   
-else:
-    print("El tipo de envío no es válido, por favor ingrese un valor entre 0 y 6")
-
+        inicial = 17900
 # Configuración de envios internacionales
 if destino == "Bolivia" or destino == "Paraguay":
     # Aumentamos un +20%
@@ -163,7 +151,8 @@ if pago == 1:
 else:
     final = int(inicial)
 
-print("\nPaís de destino del envío:", destino)
+# Salida
+print("País de destino del envío:", destino)
 print("Provincia destino:", provincia)
 print("Importe inicial a pagar:", inicial)
 print("Importe final a pagar:", final)
